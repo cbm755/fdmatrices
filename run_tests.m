ae31a0327dcf7dd90ccaea384495d7d9a8bfd42f
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2014-10-27 23:13:48 +0000

    rename test driver, change code
    untested at yet as we don't have any test scripts

--- run_tests.m
+++ run_tests.m
@@ -1,25 +1,22 @@
-setupPaths
-tests = dir('tests_unit');
-cd('tests_unit');
+files = dir('.');
 
 totaltime = cputime();
 num_tests = 0;
 num_failed = 0;
 for i=1:length(tests)
-  test = tests(i).name;
-  % detect tests b/c directory contains other stuff (e.g., surdirs and
-  % helper files)
-  if ( (~tests(i).isdir) && strncmp(test, 'test', 4) )
+  fname = files(i).name;
+  % tests should be files that start with 'test_'
+  if ( (~files(i).isdir) && strncmp(file, 'test_', 5) )
     testtime = cputime();
-    f = str2func(test(1:end-2));
+    f = str2func(fname(1:end-2));
     num_tests = num_tests + 1;
-    disp(['** Running test(s) in: ' test ]);
-    [pass,str] = f();
+    disp(['** Running test(s) in: ' fname ]);
+    pass = f();
     testtime = cputime() - testtime;
     if all(pass)
-      fprintf('** PASS: %s  [%g sec]\n', str, testtime);
+      fprintf('** PASS: %s  [%g sec]\n', fname, testtime);
     else
-      fprintf('** FAIL: %s  [%g sec]\n', str, testtime);
+      fprintf('** FAIL: %s  [%g sec]\n', fname, testtime);
       num_failed = num_failed + 1;
       pass
     end
@@ -32,4 +29,3 @@ fprintf('\n***** Passed %d/%d tests passed (%g seconds) *****\n', ...
 if (num_failed > 0)
   disp('***** WARNING: some tests failed *****');
 end
-cd('..')
